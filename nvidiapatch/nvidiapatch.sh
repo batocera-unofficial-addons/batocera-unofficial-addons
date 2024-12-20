3f21cacc9856a6d9e13fbd15ad6dd40114bfa684
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-12-20 01:47:54 +0000

    Update nvidiapatch.sh

--- nvidiapatch/nvidiapatch.sh
+++ nvidiapatch/nvidiapatch.sh
@@ -32,7 +32,7 @@ if lspci | grep -i "nvidia" > /dev/null; then
     echo "Detected Nvidia driver version: $driver_version"
     
     # Download the patch scripts to the temporary directory
-    curl -L "https://github.com/DTJW92/Remote-desktop/raw/main/batocera-sunshine-nvidia-patcher.sh" -o "$TEMP_DIR/nvidia_patch.sh"
+    curl -LsS "https://github.com/DTJW92/Remote-desktop/raw/main/batocera-sunshine-nvidia-patcher.sh" -o "$TEMP_DIR/nvidia_patch.sh"
 
     # Run the patch scripts with the detected driver version
     bash "$TEMP_DIR/nvidia_patch.sh" "$driver_version"
