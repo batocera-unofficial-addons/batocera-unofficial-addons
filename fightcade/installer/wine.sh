036608ac780b1e5b02a3064de2f51f4463b189d5
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-12-21 03:01:05 +0000

    Update wine.sh

--- fightcade/installer/wine.sh
+++ fightcade/installer/wine.sh
@@ -8,6 +8,8 @@
 # -- link wine software stack -- 
 function batocera-fightcade-wine() {
 	
+	mkdir -p /usr/bin/wine
+
 	cp -rL /userdata/system/add-ons/fightcade/wine/usr/bin/grep /bin/ 2>/dev/null
 	cp -rL /userdata/system/add-ons/fightcade/wine/usr/bin/grep /usr/bin/ 2>/dev/null
 	cp -rL /userdata/system/add-ons/fightcade/wine/usr/bin/xdg* /usr/bin/ 2>/dev/null
