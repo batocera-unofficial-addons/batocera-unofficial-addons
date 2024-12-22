682d1f7dc01ee5a45be3af5eb8e704060e7b8e74
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-12-22 03:58:55 +0000

    Update switch.sh

--- switch/switch.sh
+++ switch/switch.sh
@@ -6,11 +6,11 @@ version=$(batocera-es-swissknife --version | awk '{print $1}' | sed 's/[^0-9]*//
 # Compare version and decide which download to trigger
 if (( version == 39 || version == 40 )); then
     echo "Batocera version is 39 or 40. Triggering download for version 39/40..."
-    wait 5
+    sleep 5
     curl -L bit.ly/foclabroc-switchoff-40 | bash
 elif (( version >= 41 )); then
     echo "Batocera version is 41+. Triggering download for version 41+..."
-    wait 5
+    sleep 5
     curl -L bit.ly/foclabroc-switchoff | bash
 else
     echo "Unknown or unsupported Batocera version: $version"
