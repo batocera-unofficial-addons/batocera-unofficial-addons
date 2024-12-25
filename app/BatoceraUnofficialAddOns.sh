7e2c3ff1dad835fa015d3525a0cd9998fd851367
DTJW92 <d.weatherall92@gmail.com>
2024-12-25 18:41:30 +0000

    Uploads

---
+++ app/BatoceraUnofficialAddOns.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0  curl -L raw.githubusercontent.com/DTJW92/batocera.pro/main/app/pro.sh | bash"
+
