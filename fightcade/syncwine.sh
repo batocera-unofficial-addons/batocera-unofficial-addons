014221cf45a7cf250e9ca8980ba2cdea819498b6
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-12-26 21:03:36 +0000

    Update syncwine.sh

--- fightcade/syncwine.sh
+++ fightcade/syncwine.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash 
-dos2unix /userdata/system/add-ons/fightcade/extras/unwine.sh 2>/dev/null 
-chmod a+x /userdata/system/add-ons/fightcade/extras/unwine.sh 2>/dev/null 
+dos2unix /userdata/system/add-ons/fightcade/extra/unwine.sh 2>/dev/null 
+chmod a+x /userdata/system/add-ons/fightcade/extra/unwine.sh 2>/dev/null 
 
 # wait for fc2 to load 
 sleep 3 
@@ -10,5 +10,5 @@ while ps axg | grep -vw grep | grep -w fc2-electron > /dev/null; do
 sleep 3 
 done 
 # if fc2 is closed, unlink wine environment  
-/userdata/system/add-ons/fightcade/extras/unwine.sh 
+/userdata/system/add-ons/fightcade/extra/unwine.sh 
 exit 0; exit 1; exit 2; 
