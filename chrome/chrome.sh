d3cc102463c61c01200125a6638407687bc67415
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-12-30 23:33:53 +0000

    Update chrome.sh

--- chrome/chrome.sh
+++ chrome/chrome.sh
@@ -70,5 +70,16 @@ chmod +x /userdata/roms/ports/GoogleChrome.sh
 echo "Refreshing Ports menu..."
 curl http://127.0.0.1:1234/reloadgames
 
+# Download the image
+echo "Downloading Google Chrome logo..."
+curl -L -o /userdata/roms/ports/images/ArcadeManager_Logo.png https://github.com/DTJW92/batocera-unofficial-addons/raw/main/chrome/extra/chrome-logo.png
+echo "Adding logo to Amazon Luna entry in gamelist.xml..."
+xmlstarlet ed -s "/gameList" -t elem -n "game" -v "" \
+  -s "/gameList/game[last()]" -t elem -n "path" -v "./GoogleChrome.sh" \
+  -s "/gameList/game[last()]" -t elem -n "name" -v "Google Chrome" \
+  -s "/gameList/game[last()]" -t elem -n "image" -v "./images/chrome-logo.png" \
+  /userdata/roms/ports/gamelist.xml > /userdata/roms/ports/gamelist.xml.tmp && mv /userdata/roms/ports/gamelist.xml.tmp /userdata/roms/ports/gamelist.xml
+curl http://127.0.0.1:1234/reloadgames
+
 echo
 echo "Installation complete! You can now launch Google Chrome from the Ports menu."
