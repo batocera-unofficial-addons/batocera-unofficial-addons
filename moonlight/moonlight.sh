b0b9d79d39930234a05dadbcf5782f0e2eed3c12
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-12-30 19:04:17 +0000

    Update moonlight.sh

--- moonlight/moonlight.sh
+++ moonlight/moonlight.sh
@@ -80,10 +80,10 @@ echo "Refreshing Ports menu..."
 curl http://127.0.0.1:1234/reloadgames
 
 # Download the image
-echo "Downloading Arcade Manager logo..."
+echo "Downloading Moonlight logo..."
 curl -L -o /userdata/roms/ports/images/moonlightlogo.png https://github.com/DTJW92/batocera-unofficial-addons/raw/main/moonlight/extra/moonlightlogo.png
 
-echo "Adding logo to Arcade Manager entry in gamelist.xml..."
+echo "Adding logo to Moonlight entry in gamelist.xml..."
 xmlstarlet ed -s "/gameList" -t elem -n "game" -v "" \
   -s "/gameList/game[last()]" -t elem -n "path" -v "./Moonlight.sh" \
   -s "/gameList/game[last()]" -t elem -n "name" -v "Moonlight" \
