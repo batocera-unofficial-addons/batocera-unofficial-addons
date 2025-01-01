a07e5264054de2d01bddb41f3c26cd20a1ee132f
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2025-01-01 20:13:52 +0000

    Create heroic.sh

---
+++ heroic/heroic.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Set variables
+HEROIC_VERSION="2.15.2"
+HEROIC_URL="https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v${HEROIC_VERSION}/Heroic-${HEROIC_VERSION}.tar.xz"
+INSTALL_DIR="/userdata/system/add-ons/heroic"
+DESKTOP_FILE="/usr/share/applications/heroic.desktop"
+
+# Download Heroic
+echo "Downloading Heroic Games Launcher..."
+mkdir -p "$INSTALL_DIR"
+wget -qO- "$HEROIC_URL" | tar -xJ -C "$INSTALL_DIR" --strip-components=1
+
+# Create desktop file
+echo "Creating desktop entry for Heroic..."
+cat <<EOF > "$DESKTOP_FILE"
+[Desktop Entry]
+Type=Application
+Name=Heroic Games Launcher
+Exec=bash -c "/userdata/system/add-ons/heroic/create_game_launchers.sh &; /userdata/system/add-ons/heroic/heroic"
+Icon=/userdata/system/add-ons/heroic/resources/app.asar.unpacked/assets/icon.png
+Categories=Game;
+EOF
+
+# Make Heroic executable
+chmod +x "${INSTALL_DIR}/heroic"
+
+echo "Heroic Games Launcher setup complete! A desktop entry has been created at $DESKTOP_FILE."
