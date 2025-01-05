1cba3e3ec5ba29f9ec24fe145731bc9283eea7fd
KevoBato <lsmiththagod@gmail.com>
2025-01-05 17:45:54 -0500

    Update openrgb.sh

--- openrgb/openrgb.sh
+++ openrgb/openrgb.sh
@@ -2,7 +2,7 @@
 
 # Variables to update for different apps
 APP_NAME="OpenRGB"
-AMD_SUFFIX="x86_64_b5f46e3.AppImage"
+AMD_SUFFIX="x86_64.AppImage"
 ARM_SUFFIX=""
 LOGO_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/main/openrgb/extra/openrgb-logo.png"
 REPO_BASE_URL="https://openrgb.org/releases"
