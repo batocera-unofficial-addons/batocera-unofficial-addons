fe6a49c0fc4e96d897dc138dd5200af087b2b697
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-01-06 00:54:48 +0000

    Update brave.sh

--- brave/brave.sh
+++ brave/brave.sh
@@ -68,7 +68,7 @@ export \$(cat /proc/1/environ | tr '\0' '\n')
 export DISPLAY=:0.0
 
 # Directories and file paths
-app_dir="$ADDONS_DIR/$APP_NAME"
+app_dir="$ADDONS_DIR/${APP_NAME,,}"
 app_image="${app_dir}/${APP_NAME,,}.AppImage"
 log_dir="$LOGS_DIR"
 log_file="\${log_dir}/${APP_NAME,,}.log"
