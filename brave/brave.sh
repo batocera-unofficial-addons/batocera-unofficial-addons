833fb12c9ed11466aac6b70f46efe4f96c01a389
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-01-06 00:51:23 +0000

    Update brave.sh

--- brave/brave.sh
+++ brave/brave.sh
@@ -3,7 +3,7 @@
 # Variables to update for different apps
 APP_NAME="Brave"
 REPO="srevinsaju/Brave-AppImage"
-AMD_SUFFIX="X86_64.AppImage"
+AMD_SUFFIX="x86_64.AppImage"
 ARM_SUFFIX=""
 LOGO_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/main/brave/extra/brave-logo.png"
 
