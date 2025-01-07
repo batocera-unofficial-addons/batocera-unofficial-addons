06da2b5e405bb26297e49709ef5440ce29d08ac8
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-01-07 23:32:28 +0000

    Update hydra.sh

--- hydra/hydra.sh
+++ hydra/hydra.sh
@@ -79,22 +79,22 @@ export DISPLAY=:0.0
 
 # Directories and file paths
 app_dir="$ADDONS_DIR/${APP_NAME,,}"
-app_image="${app_dir}/${APP_NAME,,}.AppImage"
+app_image="\${app_dir}/${APP_NAME,,}.AppImage"
 log_dir="$LOGS_DIR"
-log_file="${log_dir}/${APP_NAME,,}.log"
+log_file="\${log_dir}/${APP_NAME,,}.log"
 
 # Ensure log directory exists
-mkdir -p "${log_dir}"
+mkdir -p "\${log_dir}"
 
 # Append all output to the log file
-exec &> >(tee -a "$log_file")
+exec &> >(tee -a "\$log_file")
 echo "$(date): Launching $APP_NAME"
 batocera-services start hydra
 
 # Launch AppImage
-if [ -x "${app_image}" ]; then
-    cd "${app_dir}"
-    "./${APP_NAME,,}.AppImage" --no-sandbox "$@" > "${log_file}" 2>&1
+if [ -x "\${app_image}" ]; then
+    cd "\${app_dir}"
+    "./${APP_NAME,,}.AppImage" --no-sandbox "\$@" > "\${log_file}" 2>&1
     echo "$APP_NAME exited."
 else
     echo "$APP_NAME.AppImage not found or not executable."
