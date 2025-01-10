839526cf303b8c06cbd1dc490aa84247201f7285
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-01-10 03:53:23 +0000

    Update test2.sh

--- test/test2.sh
+++ test/test2.sh
@@ -11,6 +11,7 @@ LOGS_DIR="/userdata/system/logs"
 GAME_LIST="/userdata/roms/ports/gamelist.xml"
 PORT_SCRIPT="${PORTS_DIR}/${APP_NAME}.sh"
 LOGO_PATH="${PORTS_DIR}/images/${APP_NAME,,}-logo.png"
+CONFIG_FILE="${ADDONS_DIR}/${APP_NAME,,}/ambermoon.cfg"
 
 # Step 1: Create necessary directories
 echo "Setting up directories..."
@@ -34,7 +35,15 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Step 3: Create the app launch script
+# Step 3: Update ambermoon.cfg for fullscreen
+if [ -f "$CONFIG_FILE" ]; then
+    echo "Configuring $APP_NAME for fullscreen mode..."
+    sed -i 's/"Fullscreen": false,/"Fullscreen": true,/' "$CONFIG_FILE"
+else
+    echo "Configuration file not found. Skipping fullscreen configuration."
+fi
+
+# Step 4: Create the app launch script
 echo "Creating launch script..."
 cat << EOF > "$PORT_SCRIPT"
 #!/bin/bash
@@ -50,15 +59,15 @@ EOF
 
 chmod +x "$PORT_SCRIPT"
 
-# Step 4: Refresh the Ports menu
+# Step 5: Refresh the Ports menu
 echo "Refreshing Ports menu..."
 curl http://127.0.0.1:1234/reloadgames
 
-# Step 5: Download the logo
+# Step 6: Download the logo
 echo "Downloading logo..."
 curl -L -o "$LOGO_PATH" "$LOGO_URL"
 
-# Step 6: Add entry to gamelist.xml
+# Step 7: Add entry to gamelist.xml
 echo "Adding $APP_NAME to gamelist.xml..."
 xmlstarlet ed -s "/gameList" -t elem -n "game" -v "" \
   -s "/gameList/game[last()]" -t elem -n "path" -v "./${APP_NAME}.sh" \
@@ -66,5 +75,6 @@ xmlstarlet ed -s "/gameList" -t elem -n "game" -v "" \
   -s "/gameList/game[last()]" -t elem -n "image" -v "./images/${APP_NAME,,}-logo.png" \
   "$GAME_LIST" > "$GAME_LIST.tmp" && mv "$GAME_LIST.tmp" "$GAME_LIST"
 curl http://127.0.0.1:1234/reloadgames
+
 echo
 echo "Installation complete! You can now launch $APP_NAME from the Ports menu."
