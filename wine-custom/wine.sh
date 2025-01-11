5ccebb853ab87c3d5d591d485dc13e6a24930e24
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-01-11 03:48:08 +0000

    Update wine.sh

--- wine-custom/wine.sh
+++ wine-custom/wine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the options
-OPTIONS=("1" "Wine & Proton (vanilla/regular)" "2" "Wine-TKG-Staging" "3" "Wine-GE Custom" "4" "GE-Proton" "5" "Steamy-AIO Wine Dependency Installer")
+OPTIONS=("1" "Wine & Proton (vanilla/regular)" "2" "Wine-TKG-Staging" "3" "Wine-GE Custom" "4" "GE-Proton" "5" "Steamy-AIO Wine Dependency Installer" "6" "V40 Stock Wine")
 
 # Use dialog to display the menu
 CHOICE=$(dialog --clear --backtitle "Wine Installation" \
@@ -18,25 +18,28 @@ clear
 case $CHOICE in
     1)
         echo "You chose Wine Vanilla and Proton."
-        curl -L https://github.com/uureel/batocera.pro/raw/main/wine-custom/vanilla.sh | bash
+        curl -L https://github.com/DTJW92/batocera-unofficial-addons/raw/main/wine-custom/vanilla.sh | bash
         ;;
     2)
         echo "You chose Wine-tkg staging."
-        curl -L https://github.com/uureel/batocera.pro/raw/main/wine-custom/tkg.sh | bash
+        curl -L https://github.com/DTJW92/batocera-unofficial-addons/raw/main/wine-custom/tkg.sh | bash
         ;;
 
     3)
         echo "You chose Wine-GE Custom."
-        curl -L  https://github.com/uureel/batocera.pro/raw/main/wine-custom/wine-ge.sh | bash
+        curl -L  https://github.com/DTJW92/batocera-unofficial-addons/raw/main/wine-custom/wine-ge.sh | bash
         ;;
     4)
         echo "You chose GE-Proton."
-        curl -L  https://github.com/uureel/batocera.pro/raw/main/wine-custom/ge-proton.sh | bash
+        curl -L  https://github.com/DTJW92/batocera-unofficial-addons/raw/main/wine-custom/ge-proton.sh | bash
         ;;
     5)
         echo "You chose Steamy-AIO."
-        curl -L  https://github.com/uureel/batocera.pro/raw/main/wine-custom/steamy.sh | bash
-        ;;    
+        curl -L  https://github.com/DTJW92/batocera-unofficial-addons/raw/main/wine-custom/steamy.sh | bash
+        ;;
+    6)
+        echo "You chose V40 stock wine."
+        curl -L https://github.com/DTJW92/batocera-unofficial-addons/raw/main/v40wine/v40wine.sh
      *)
         echo "Invalid choice or no choice made. Exiting."
         ;;
