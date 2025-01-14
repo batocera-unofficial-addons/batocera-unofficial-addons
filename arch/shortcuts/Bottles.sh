7f07bd284cdfda6c04533c52fbdf5978ab42e7ad
trashbus99 <csmandrew@live.com>
2025-01-14 16:16:28 +0200

    Update Bottles.sh

--- arch/shortcuts/Bottles.sh
+++ arch/shortcuts/Bottles.sh
@@ -17,7 +17,7 @@ batocera-mouse show
           --bind /userdata /userdata \
           --bind /newroot /newroot \
           --bind / /batocera \
-  bash -c 'prepare && source /opt/env && dbus-run-session boilr-gui '"${@}"''
+  bash -c 'prepare && source /opt/env && dbus-run-session bottles '"${@}"''
 #------------------------------------------------
 # batocera-mouse hide
 #------------------------------------------------
