dccc6a61a2765cbfdf50ab89092b362fd35adfdb
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-02-06 02:34:11 +0000

    Update zelda2.sh

--- windows/zelda2.sh
+++ windows/zelda2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your variables for easy customization
-URL="https://github.com/trashbus99/profork/releases/download/r1/Zelda2_Remake.wsquashfs"
+URL="https://github.com/DTJW92/batocera-unofficial-addons/releases/download/AppImages/Zelda2_Remake.wsquashfs"
 KEYS_URL=""  # Leave empty if no keys file is needed
 DEST_DIR="/userdata/roms/windows"
 MESSAGE="Zelda 2 PC Remake - Needs Enable DXVK in Adv settings for game and change video mode resolution to 1280x720x60 for best results"  # Leave empty if no message is needed
