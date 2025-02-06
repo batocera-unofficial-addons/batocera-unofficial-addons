68c987c521983bc941ec77468ab2f2616dda1ed7
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-02-06 02:47:13 +0000

    Update zeldadoi.sh

--- windows/zeldadoi.sh
+++ windows/zeldadoi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your variables for easy customization
-URL="https://github.com/trashbus99/profork/releases/download/r1/Zelda-Dungeons_of_Infinity.wsquashfs"
+URL="https://github.com/DTJW92/batocera-unofficial-addons/releases/download/AppImages/Zelda.-.Dungeons.of.Infinity.wsquashfs"
 KEYS_URL=""  # Leave empty if no keys file is needed
 DEST_DIR="/userdata/roms/windows"
 MESSAGE=""  # Leave empty if no message is needed
