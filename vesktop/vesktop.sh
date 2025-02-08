c853eb87987182fb932ae1845e3684fca1254795
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-02-08 03:39:54 +0000

    Update vesktop.sh

--- vesktop/vesktop.sh
+++ vesktop/vesktop.sh
@@ -146,12 +146,8 @@ EOF
 chmod +x "/userdata/system/configs/vesktop/restore_desktop_entry.sh"
 
 # Add to startup
-cat <<EOF > "/userdata/system/custom.sh"
-#!/bin/bash
-# Restore Chiaki desktop entry at startup
-bash /userdata/system/configs/vesktop/restore_desktop_entry.sh &
-EOF
-chmod +x "/userdata/system/custom.sh"
+echo "bash /userdata/system/configs/vesktop/restore_desktop_entry.sh &" >> /userdata/system/custom.sh
+chmod +x /userdata/system/custom.sh
 
 echo "Refreshing Ports menu..."
 curl http://127.0.0.1:1234/reloadgames
