3d2a3c6952358721a064bb63722e00cf9165158d
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-03-25 05:26:52 +0000

    Update archkde.sh

--- docker/archkde.sh
+++ docker/archkde.sh
@@ -51,7 +51,7 @@ docker run -d \
 
 # Final message
 if docker ps -q -f name=arch-kde &> /dev/null; then
-    MSG="${APPNAME} container has been started successfully.\n\nAccess it via: http://<your-ip>:3000\n\nPersistent data is stored in:\n$config_dir\nand\n$config_dir/home"
+    MSG="${APPNAME} container has been started successfully.\n\nAccess it via: http://<your-ip>:3000\n\nPersistent data is stored in:\n$data_dir"
 else
     MSG="Failed to start ${APPNAME}. Please check Docker logs for troubleshooting."
 fi
