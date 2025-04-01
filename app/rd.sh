1e43dc1628d5d1bf0dfb02f5fbb2701bab3ef166
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-04-01 03:33:41 +0100

    Create rd.sh

---
+++ app/rd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "This bit.ly link will stop working soon. The new install command will be;"
+echo ""
+echo "curl -L install.batoaddons.app | bash"
+sleep 5
+echo ""
+echo "Now redirecting to continue instalation..."
+sleep 2
+curl -fsSL install.batoaddons.app | bash
