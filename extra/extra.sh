05ece6d4ea6358c7d7275aea43a3a70fc3619aaf
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-04-03 01:39:15 +0100

    Create extra.sh

---
+++ extra/extra.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#!/bin/bash
+
+# Get Batocera version
+version=$(batocera-es-swissknife --version | awk '{print $1}' | sed 's/[^0-9]*//g')
+
+# Check if version is one of the supported ones
+case "$version" in
+    38|39|40|41)
+        echo "Detected Batocera version: $version"
+        mkdir -p /userdata/extra && \
+        wget -O - "https://github.com/git-developer/batocera-extra/tarball/batocera-$version" | \
+        gunzip | tar x --strip-components 1 -C /userdata/extra
+        ;;
+    *)
+        echo "Unsupported or unknown Batocera version: $version"
+        exit 1
+        ;;
+esac
+
+ mkdir -p /userdata/extra && wget -O - https://github.com/git-developer/batocera-extra/tarball/main | gunzip | tar x --strip-components 1 -C /userdata/extra
