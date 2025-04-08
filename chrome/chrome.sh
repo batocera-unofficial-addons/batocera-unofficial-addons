c79ed864ff0d8d68df97c5bbfeca50764a21e6ae
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-04-08 14:07:49 +0100

    Update chrome.sh

--- chrome/chrome.sh
+++ chrome/chrome.sh
@@ -6,7 +6,7 @@ arch=$(uname -m)
 
 if [ "$arch" == "x86_64" ]; then
     echo "Architecture: x86_64 detected."
-appimage_url=$(curl -s https://api.github.com/repos/ivan-hc/Chrome-appimage/releases/latest | jq -r ".assets[] | select(.name | endswith(\".AppImage\")) | .browser_download_url" | grep stable)
+appimage_url=$(curl -s https://api.github.com/repos/ivan-hc/Chrome-appimage/releases/latest | jq -r ".assets[] | select(.name | endswith(\".AppImage\")) | .browser_download_url" | grep stable)?
 else
     echo "Unsupported architecture: $arch. Exiting."
     exit 1
