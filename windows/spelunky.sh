56b58e5e9860946a0687be1d6bf244311b9e6b4d
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-04-10 17:54:28 +0100

    Update spelunky.sh

--- windows/spelunky.sh
+++ windows/spelunky.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your variables for easy customization
-URL="https://github.com/trashbus99/profork/releases/download/r1/spelunky.wsquashfs"
+URL="https://github.com/DTJW92/batocera-unofficial-addons/releases/download/AppImages/spelunky.wsquashfs"
 KEYS_URL=""  # Leave empty if no keys file is needed
 DEST_DIR="/userdata/roms/windows"
 MESSAGE=""  # Leave empty if no message is needed
