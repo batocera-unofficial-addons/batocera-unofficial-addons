a3c8d6e89c7928f54414675322bf60289848533d
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-04-16 14:32:58 +0100

    Update install.sh

--- app/install.sh
+++ app/install.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 
-# Check for Profork
-if [[ -e "/userdata/system/pro" ]]; then
-    cat <<'EOF' > /dev/tty
-
-Cliffy has decided to act like a child for an unknown reason.
-I haven't touched his repo whatsoever, but he has decided to mess with BUA's launching script.
-What I once believed would be a great collaboration partnership to work on a viable Batocera Pro replacement
-has instead turned into a school playground. The only problem is, I'm not in that playground, so it's just
-Cliffy throwing toys around for no reason. Don't know what's up with you bro, but I ain't got time for it.
-
-Unfortunately he's decided, despite no incompatibility, that BUA cannot be installed alongside Profork.
-Bottom line: Profork should not be rewriting launchers that don’t belong to it, deleting itself in protest,
-or turning user tools into a passive-aggressive circus. This is toxic behavior, and it undermines trust in the project entirely.
-If this was meant as satire or commentary, it needs to stop. Let your work stand on its own without sabotaging others.
-
-Please note: if you continue with this installation, Profork will no longer open, as Cliffy added a detection
-code and a "lock" file. Don't ask me why — this is literally the only thing in the entire BUA repo that even
-has any reference to Profork. But hey, I'm not here to tell people how to run their own repos...
-just don't fuck with mine.
-
-EOF
-
-    echo -n "Type 'y' and press Enter to continue, or anything else to exit: " > /dev/tty
-    read confirm < /dev/tty
-
-    if [[ "${confirm,,}" != "y" ]]; then
-        echo -e "\nExiting..." > /dev/tty
-        sleep 2
-        clear
-        exit 0
-    fi
-fi
-
 # URLs
 AMD64="https://github.com/DTJW92/batocera-unofficial-addons/raw/refs/heads/main/app/install_x86.sh"
 ARM64="https://github.com/DTJW92/batocera-unofficial-addons/raw/refs/heads/main/app/install_arm64.sh"
