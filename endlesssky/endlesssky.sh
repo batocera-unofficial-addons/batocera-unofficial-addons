165da054595deabed407515e799421a1760e6257
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-05-22 17:43:35 +0100

    Update endlesssky.sh

--- endlesssky/endlesssky.sh
+++ endlesssky/endlesssky.sh
@@ -86,7 +86,7 @@ curl http://127.0.0.1:1234/reloadgames
 
 # Download the image
 echo "Downloading Endless Sky logo..."
-curl -L -o /userdata/roms/ports/images/endlessskylogo.png https://github.com/DTJW92/batocera-unofficial-addons/raw/main/endlesssky/extra/endlessskylogo.png
+curl -L -o /userdata/roms/ports/images/endlessskylogo.png https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/raw/main/endlesssky/extra/endlessskylogo.png
 
 echo "Adding logo to Arcade Manager entry in gamelist.xml..."
 xmlstarlet ed -s "/gameList" -t elem -n "game" -v "" \
