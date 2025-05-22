ad0119dc7b739e0efe064e81567348b3808f44b8
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-05-22 18:22:30 +0100

    Update cbbit.sh

--- windows/cbbit.sh
+++ windows/cbbit.sh
@@ -2,13 +2,13 @@
 #!/bin/bash
 
 # Define your variables for easy customization
-URL="https://github.com/DTJW92/batocera-unofficial-addons/releases/download/AppImages/CrashBandicoot-BackInTime.wsquashfs"
+URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/releases/download/AppImages/CrashBandicoot-BackInTime.wsquashfs"
 KEYS_URL=""  # Leave empty if no keys file is needed
 DEST_DIR="/userdata/roms/windows"
 MESSAGE=""  # Leave empty if no message is needed
 GAME_LIST="/userdata/roms/windows/gamelist.xml"
 APP_NAME="Crash Bandicoot - Back In Time"
-LOGO_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/main/windows/extra/crash.jpg"
+LOGO_URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/raw/main/windows/extra/crash.jpg"
 LOGO_PATH="/userdata/roms/windows/images/crash-logo.jpg"
 
 # Ensure destination directory exists
