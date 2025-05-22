8ae3a13e9f8e742ecd7c2e286847368f65c93459
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-05-22 18:04:11 +0100

    Update netflix.sh

--- netflix/netflix.sh
+++ netflix/netflix.sh
@@ -4,12 +4,12 @@
 APPNAME="Netflix"
 APPDIR="/userdata/system/add-ons/google-chrome"
 APPPATH="$APPDIR/GoogleChrome.AppImage"
-CHROME_INSTALLER_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/refs/heads/main/chrome/chrome.sh"
+CHROME_INSTALLER_URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/raw/refs/heads/main/chrome/chrome.sh"
 PORT_SCRIPT="/userdata/roms/ports/Netflix.sh"
 ICON_PATH="/userdata/roms/ports/images/netflix-logo.jpg"
 KEYS_PATH="/userdata/roms/ports/Netflix.sh.keys"
-LOGO_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/main/netflix/extra/netflix-logo.jpg"
-KEYS_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/refs/heads/main/netflix/extra/Netflix.sh.keys"
+LOGO_URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/raw/main/netflix/extra/netflix-logo.jpg"
+KEYS_URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/raw/refs/heads/main/netflix/extra/Netflix.sh.keys"
 GAMELIST="/userdata/roms/ports/gamelist.xml"
 
 # Step 1: Show dialog to confirm
