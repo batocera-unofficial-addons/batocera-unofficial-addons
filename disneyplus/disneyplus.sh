b88232f0ead52a98af3cfedd4110845fbdac37a6
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-05-22 17:26:34 +0100

    Update disneyplus.sh

--- disneyplus/disneyplus.sh
+++ disneyplus/disneyplus.sh
@@ -4,12 +4,12 @@
 APPNAME="Disney Plus"
 APPDIR="/userdata/system/add-ons/google-chrome"
 APPPATH="$APPDIR/GoogleChrome.AppImage"
-CHROME_INSTALLER_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/refs/heads/main/chrome/chrome.sh"
+CHROME_INSTALLER_URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/raw/refs/heads/main/chrome/chrome.sh"
 PORT_SCRIPT="/userdata/roms/ports/DisneyPlus.sh"
 ICON_PATH="/userdata/roms/ports/images/disneyplus-logo.png"
 KEYS_PATH="/userdata/roms/ports/DisneyPlus.sh.keys"
-LOGO_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/main/disneyplus/extra/disneyplus-logo.png"
-KEYS_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/refs/heads/main/netflix/extra/Netflix.sh.keys"
+LOGO_URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/raw/main/disneyplus/extra/disneyplus-logo.png"
+KEYS_URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/raw/refs/heads/main/netflix/extra/Netflix.sh.keys"
 GAMELIST="/userdata/roms/ports/gamelist.xml"
 
 # Step 1: Show dialog to confirm
