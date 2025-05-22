dae322a66f959a7cebe90e9826811675fda6a319
The Notorious F.O.X <96082560+DTJW92@users.noreply.github.com>
2025-05-22 18:25:18 +0100

    Update srb2.sh

--- windows/srb2.sh
+++ windows/srb2.sh
@@ -2,13 +2,13 @@
 #!/bin/bash
 
 # Define your variables for easy customization
-URL="https://github.com/DTJW92/batocera-unofficial-addons/releases/download/AppImages/SRB2.wsquashfs"
+URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/releases/download/AppImages/SRB2.wsquashfs"
 KEYS_URL=""  # Leave empty if no keys file is needed
 DEST_DIR="/userdata/roms/windows"
 MESSAGE=""  # Leave empty if no message is needed
 GAME_LIST="/userdata/roms/windows/gamelist.xml"
 APP_NAME="Sonic Robo Blast 2"
-LOGO_URL="https://github.com/DTJW92/batocera-unofficial-addons/raw/main/windows/extra/srb2-logo.png"
+LOGO_URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addons/raw/main/windows/extra/srb2-logo.png"
 LOGO_PATH="/userdata/roms/windows/images/srb2-logo.png"
 
 # Ensure destination directory exists
