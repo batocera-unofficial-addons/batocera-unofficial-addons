087208b72ec5c93cf149c579512b1fd4415e89f2
DRLEdition19 <drledition19@gmail.com>
2025-06-21 01:34:00 -0300

    Update f1.sh

--- f1/f1.sh
+++ f1/f1.sh
@@ -105,7 +105,7 @@ echo "Adding $APP_NAME to gamelist.xml..."
 xmlstarlet ed -s "/gameList" -t elem -n "game" -v "" \
   -s "/gameList/game[last()]" -t elem -n "path" -v "./${APP_NAME}.sh" \
   -s "/gameList/game[last()]" -t elem -n "name" -v "$APP_NAME" \
-  -s "/gameList/game[last()]" -t elem -n "image" -v "./images/${APP_NAME,,}-logo.jpg" \
+  -s "/gameList/game[last()]" -t elem -n "image" -v "./images/${APP_NAME}-logo.jpg" \
   "$GAME_LIST" > "$GAME_LIST.tmp" && mv "$GAME_LIST.tmp" "$GAME_LIST"
 
 # Step 5: Final refresh
