618cc4fd931671c8d5093a4a28455df2771c28e3
DRLEdition19 <drledition19@gmail.com>
2025-06-21 00:29:02 -0300

    Update f1.sh

--- f1/f1.sh
+++ f1/f1.sh
@@ -6,6 +6,49 @@ LOGO_URL="https://github.com/batocera-unofficial-addons/batocera-unofficial-addo
 LOGO_PATH="/userdata/roms/ports/images/${APP_NAME,,}-logo.jpg"
 GAME_LIST="/userdata/roms/ports/gamelist.xml"
 
+# Temporary directory for download
+TEMP_DIR="/userdata/tmp/Antimicrox"
+DRL_FILE="$TEMP_DIR/Antimicrox.DRL"
+EXTRACT_DIR="$TEMP_DIR/extracted"
+DEST_DIR="/"
+PORTS_DIR="/userdata/roms/ports"
+
+# Create the temporary directories
+echo "Creating temporary directories..."
+mkdir -p $TEMP_DIR
+mkdir -p $EXTRACT_DIR
+mkdir -p $PORTS_DIR
+clear
+
+# Download the DRL file
+echo "Downloading the DRL file..."
+curl -L -o $DRL_FILE "https://github.com/DRLEdition19/DRLEdition_Interface/releases/download/files/Antimicrox.DRL"
+
+# Check if download was successful
+if [ ! -f "$DRL_FILE" ]; then
+    echo "Error: Failed to download DRL file"
+    exit 1
+fi
+
+# Extract the squashfs file
+echo "Extracting the DRL file..."
+unsquashfs -f -d "$EXTRACT_DIR" "$DRL_FILE"
+
+# Check if extraction was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to extract the DRL file"
+    rm -rf $TEMP_DIR
+    exit 1
+fi
+
+# Copia forçada dos arquivos extraídos para o diretório de destino, com sobrescrita
+echo "Copying files to the system (forced overwrite)..."
+cp -rf "$EXTRACT_DIR"/* "$DEST_DIR"
+
+# Limpeza
+echo "Cleaning up..."
+rm -rf "$TEMP_DIR"
+
 # Step 1: Create the launcher script
 echo "Creating ${APP_NAME}.sh..."
 cat << 'EOF' > "/userdata/roms/ports/${APP_NAME}.sh"
@@ -59,6 +102,11 @@ exit 0
 EOF
 
 chmod +x "${APP_NAME}.sh"
+
+# Salva alterações
+echo "Saving changes..."
+batocera-save-overlay
+clear
 
 # Step 2: Refresh the Ports menu
 echo "Refreshing Ports menu..."
